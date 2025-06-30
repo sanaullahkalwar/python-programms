6612ea12f03bc36058121621862d3444e4d8a38f
sanaullahkalwar <sanullahkalwar@gmail.com>
2025-06-30 19:42:48 +0500

    Collections data types

---
+++ CollectionDataTypes/Lists/Lists.py
@@ -0,0 +1,20 @@
+#this file contains the definition and programs related to lists in the python
+
+'''
+There are four collection data types in the Python programming language:
+
+    List is a collection which is ordered and changeable. Allows duplicate members.
+    Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+    Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+    Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+1:
+lists are used to store multiple items in a single variable
+lists are ordered, changable and may store duplicate 
+'''
+theList = ["sanaullah","ali","mujeeb","maya","mohsin","sanaullah"]
+print(theList)
+
+
+#Print the length of list
+print(f"The length is = {len(theList)}")
